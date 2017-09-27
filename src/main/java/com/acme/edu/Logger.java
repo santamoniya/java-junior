77e7b3f7d8150699ad9d868cafb6853cbac336a6
santamoniya <santamoniya@mail.ru>
2017-09-27 17:15:51 +0300

    Added char

--- src/main/java/com/acme/edu/Logger.java
+++ src/main/java/com/acme/edu/Logger.java
@@ -1,11 +1,14 @@
 package com.acme.edu;
 
 public class Logger {
+
+    private static final String PRIMITIVE_PREFIX = "primitive: ";
+
     public static void log(int message) {
-        System.out.println("primitive: " + message);
+        System.out.println(PRIMITIVE_PREFIX + message);
     }
 
     public static void log(byte message) {
-        System.out.println("primitive: " + message);
+        System.out.println(PRIMITIVE_PREFIX + message);
     }
 }
